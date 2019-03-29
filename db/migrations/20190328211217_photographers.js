0821e3fded4270ba15c697e7726d1b039a31d8dc
fatuma ismail <timaismail90@gmail.com>
2019-03-29 00:43:11 +0000

    seeded influencers and photographers

--- db/migrations/20190328211217_photographers.js
+++ db/migrations/20190328211217_photographers.js
@@ -3,11 +3,12 @@ exports.up = function(knex, Promise) {
     knex.schema.CreateTable('photographers', function (table) {
         table.increment('id').unsigned().primary();
         table.string('name');
-        table.json('profile');
+        table.string('profile_image');
+        table.string('location');
+        table.string('location');
     })
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema.dropTable('photographers');
 };
- 
