4d0506fb90f29473f2dd140af5804cfc3456aa49
Ken Thooptong <ken.thooptong@gmail.com>
2019-03-30 19:37:42 +0000

    Create home and upload page. Need to upload background photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import '../home.css'
+import {Form, Row, Col, Button} from 'react-bootstrap';
 
 const Home = () => {
   return (
-  <div>
-      <p>Home</p>
-  </div>
-  );
+
+      <div className="home-page">
+
+        <h1 className="title"> COLLAB</h1>
+        <h2 className="subtitle">Collaborate for the sake of art</h2>
+
+      <div className="login">
+      <Form>
+        <Form.Group as={Row} controlId="formPlaintextEmail">
+        <Col>
+        <Form.Control plaintext defaultValue="Enter your email" />
+        </Col>
+        </Form.Group>
+
+      <Form.Group as={Row} controlId="formPlaintextPassword">
+        <Col>
+        <Form.Control type="password" placeholder="Password" />
+      </Col>
+      </Form.Group>
+      </Form>
+
+      <Button variant="outline-success" size="lg">
+      Login
+      </Button>
+
+      </div>
+      <img src="../collabhomepage-copy.jpg" />
+      </div>
+  )
 };
 
 export default Home;
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../upload.css'
+import {Form, Row, Col, Button} from 'react-bootstrap';
 
 const Upload = () => {
   return (
-  <div >
-    <p > Upload </p>
+  <div className="upload-page">
+
+    <p className="upload"> Which styles are you feeling today? </p>
+    <form action='/api/images' method="post" enctype="multipart/form-data">
+    <input type='file' name='image' className="upload-photo" />
+    </form>
+     <img src="../collabupload-copy.jpg" />
     </div>
   );
 };
