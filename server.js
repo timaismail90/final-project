5fab960d957a31f0bb93f7231f26fea7a13045d9
Halimo <halimoghelle@gmail.com>
2019-03-31 20:35:01 +0000

    implemented node.js express server for react

---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const path = require('path');
+const port = 5000;
+const app = express();
+
+
+
+app.listen(port);
+
+console.log('App is listening on port ' + port);
