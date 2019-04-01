e22ebbc292c2ef9350160958ef0934b3ebafba6f
Ken Thooptong <ken.thooptong@gmail.com>
2019-04-01 19:17:20 +0000

    change to cards for result page

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../results.css'
-import {Container, Row, Col, Button, Carousel} from 'react-bootstrap';
+import {Container, Row, Col, Button, Carousel, Card} from 'react-bootstrap';
 import carouselOne from '../carousel1.jpg';
 import carouselTwo from '../carousel2.jpg';
 import carouselThree from '../carousel3.jpg';
@@ -30,20 +30,81 @@ const Results = () => {
           src={carouselThree}
         />
       </Carousel.Item>
-    </Carousel>;
+    </Carousel>
     </div>
+
+  <h2 className="title-found">Photographers Who Match</h2>
 
     <Container>
       <Row>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" /></Col>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1547285864-d3aa7596b175?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80" /></Col>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1547825727-ae653b92ea52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" /></Col>
+
+    <Col sm>
+      <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+
+    <Col sm>
+      <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+
+    <Col sm>
+      <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
       </Row>
 
       <Row>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1543307711-f083bc0afbfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80" /></Col>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1543405075-f0d9e0457476?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" /></Col>
-    <Col sm><img className="result" src="https://images.unsplash.com/photo-1524840236425-0e7905b6f19a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" /></Col>
+    <Col sm>
+      <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+
+    <Col sm>
+      <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+
+    <Col sm>
+       <Card style={{ width: '18rem' }}>
+        < Card.Img variant = "top"
+        src = "https://images.unsplash.com/photo-1548353396-ee9afe09f4bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80" / >
+        <Card.Body>
+          <Card.Text>Hal Gal </Card.Text>
+          <Button variant="outline-success">Collab!</Button>
+        </Card.Body>
+      </Card>
+    </Col>
       </Row>
     </Container>;
   </div>
