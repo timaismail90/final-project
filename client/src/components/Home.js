bd1c2f0356bbddde68c70cde64fcdb8f567c68cc
fatuma ismail <timaismail90@gmail.com>
2019-04-02 17:35:39 +0000

    removed firebase

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,46 +3,50 @@ import '../home.css'
 import {Form, Row, Col, Button, Container} from 'react-bootstrap';
 import backgroundHome from '../collabhomepageanimated.gif';
 
-const Home = () => {
+
+class Home extends Component {
+render () {
   return (
 
-  <div>
-  <Container>
-    <Row>
-      <Col sm={4}>
-
-        <h1 className="title"> COLLAB</h1>
-        <h2 className="subtitle">Collaborate for the sake of art</h2>
-
-
-      <Form>
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-        <Col>
-        <Form.Control plaintext defaultValue="Enter your email" />
+    <div>
+    <Container>
+      <Row>
+        <Col sm={4}>
+  
+          <h1 className="title"> COLLAB</h1>
+          <h2 className="subtitle">Collaborate for the sake of art</h2>
+  
+  
+        <Form>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+          <Col>
+          <Form.Control plaintext defaultValue="Enter your email" />
+          </Col>
+          </Form.Group>
+  
+        <Form.Group as={Row} controlId="formPlaintextPassword">
+          <Col>
+          <Form.Control type="password" placeholder="Password" />
         </Col>
         </Form.Group>
+        </Form>
+  
+        <Button variant="outline-success" size="lg" className="loginButton">
+        Login
+        </Button>
+  
+        </Col>
+  
+        <Col sm={8}>
+          <img className="homePhoto" src={backgroundHome} />
+        </Col>
+        </Row>
+      </Container>
+  
+    </div>
+    )
+  };
 
-      <Form.Group as={Row} controlId="formPlaintextPassword">
-        <Col>
-        <Form.Control type="password" placeholder="Password" />
-      </Col>
-      </Form.Group>
-      </Form>
-
-      <Button variant="outline-success" size="lg" className="loginButton">
-      Login
-      </Button>
-
-      </Col>
-
-      <Col sm={8}>
-        <img className="homePhoto" src={backgroundHome} />
-      </Col>
-      </Row>
-    </Container>
-
-  </div>
-  )
-};
+}
 
 export default Home;
