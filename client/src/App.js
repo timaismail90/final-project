36724b86154012acbac7872e0f92ecc023b26f0e
Halimo <halimoghelle@gmail.com>
2019-04-05 00:45:50 +0000

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -12,19 +12,31 @@ import Select from "./components/Select.js"
 import Messages from "./components/Messages.js"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     user: [],
+     file: null
+    };
+  }
+
+
+  
+  
   render() {
     return (
 
       <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/select" component={Select} exact />
-        <Route path="/loading" component={Loading} />
-        <Route path="/upload" component={Upload} />
-        <Route path="/photographer" component={Photographer} />
-        <Route path="/results" component={Results} />
-        <Route path="/requests" component={Requests} />
-        <Route path="/messages" component={Messages} />
+        <Route path="/" User = {this.state.user} component={Home} exact />
+        <Route path="/select" User = {this.state.user} component={Select} exact />
+        <Route path="/loading" User = {this.state.user} component={Loading} />
+        <Route path="/upload" User = {this.state.user} component={Upload} />
+        <Route path="/photographer" User = {this.state.user} component={Photographer} />
+        <Route path="/results" User = {this.state.user} component={Results} />
+        <Route path="/requests" User = {this.state.user} component={Requests} />
+        <Route path="/messages" User = {this.state.user} component={Messages} />
         <Route component={Error} />
       </Switch>
       </BrowserRouter>
